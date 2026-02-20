83cec93c5b19d507ca853d6bc3fd7e9a7106ec35
Alfredoperry14 <37465263+Alfredoperry14@users.noreply.github.com>
2026-02-20 12:31:05 -0800

    Problem 2 Done

--- Homework2/problem2.py
+++ Homework2/problem2.py
@@ -1,10 +1,16 @@
+#Problem 2
 import numpy as np
-P = [0.49, 0.105, 0.105, 1.05, 1.05, 0.0225, 0.0225, 0.0225, 0.0225]
-print(P.count())
-
-
+P = [0.49, 0.105, 0.105, 0.105, 0.105, 0.0225, 0.0225, 0.0225, 0.0225]
+print(len(P), 'probabilities')
+print('The sum of all of the probabilities was', np.sum(P))
 ans = 0
-for prob in P:
+print("Entropy is the negative of the total sum of each probability multiplied by log2(probability)")
+for probIndex, prob in enumerate(P):
+    #Starts with ans = 0 + 0.49 * np.log2(0.49)
     ans += prob * np.log2(prob)
-    print("Entropy at S,", P.index(prob), "is", ans)
+    #print(f"Entropy sum at S{probIndex} is", ans)
 ans = -ans
+
+#Both are the same answer
+#print(-np.sum(P * np.log2(P)))
+print('Entropy is', ans)
